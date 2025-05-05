8ecdd56ac26b206960c86d2cf2be592f43ebae3c
tienpvse1 <good66612@gmail.com>
2025-05-05 23:26:30 +0700

    chore: add fzf for better file match

--- lua/configs/telescope/dependencies.lua
+++ lua/configs/telescope/dependencies.lua
@@ -2,6 +2,10 @@ return {
 	{
 		"nvim-telescope/telescope-live-grep-args.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
 		-- This will not install any breaking changes.
 		-- For major updates, this must be adjusted manually.
 		version = "^1.0.0",
